0402586e00b0a81e826bb4ae23601407fc94ddb9
Adriana Sveca <svecaadriana05gmail.com>
2026-08-03 18:56:50 +0100

    Add styled login/register pages and begin authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -11,6 +10,18 @@ class AuthController extends Controller
 
     }
     public function register(Request $request){
+        $name = $request->name;
+        $request->validate([
+            'name'=>'required|min:3',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:8',
+
+        ]);
+         User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
 
     }
 }
